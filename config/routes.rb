6c2f09208eef13c9afc5e1f955d63651cb77e8c5
Asura <luodaoyi@gmail.com>
2018-06-10 23:59:49 +0800

    add simple admin

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,14 @@ Rails.application.routes.draw do
   resources :users
   resources :sessions
 
+  namespace :admin do
+    root 'users#index'
+
+    resources :users do
+      collection do
+        get :search
+      end
+    end
+  end
+
 end
