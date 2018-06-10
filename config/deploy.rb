a82139b6846ab84ba4aca220f44c8487b7ce8c17
Asura <luodaoyi@gmail.com>
2018-06-10 15:05:35 +0800

    add travis to auto deploy add .travis.yml file

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,3 +47,4 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+set :ssh_options, keys: ["config/deploy_id_rsa"] if File.exist?("config/deploy_id_rsa")
